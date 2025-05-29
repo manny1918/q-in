d57aa3a01a7e53f53b8aa095ff8e5e254d88cb6b
happylearning17 <pedrolopez@amazon.com>
2025-05-29 20:50:23 +0100

    setup jwt

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require('express-async-handler');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
 
 const User = require('../models/userModel');
 
@@ -30,6 +31,7 @@ const registerUser = asyncHandler(async (req, res) => {
       _id: user._id,
       name: user.name,
       email: user.email,
+      token: generateToken(user._id)
     });
   } else {
     res.status(400);
@@ -46,6 +48,7 @@ const loginUser = asyncHandler(async (req, res) => {
       _id: user._id,
       name: user.name,
       email: user.email,
+      token: generateToken(user._id)
     });
   } else {
     res.status(400);
@@ -53,6 +56,12 @@ const loginUser = asyncHandler(async (req, res) => {
   }
   res.send('Login route');
 });
+
+const generateToken=(id)=>{
+  return jwt.sign({id}, process.env.JWT_SECRET, {
+    expiresIn: '30d'
+  })
+}
 
 module.exports = {
   registerUser,
