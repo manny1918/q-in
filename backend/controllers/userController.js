0db32370705cce2c584693c143c03c946d9fb8b9
happylearning17 <pedrolopez@amazon.com>
2025-05-29 20:04:57 +0100

    error handling

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,9 @@
 const registerUser = (req, res)=>{
-    res.send('REgister route')
+    const {name, email, password} = req.body
+    if(!name || !email || !password){
+        throw new Error('Please include all the data')
+    }
+    res.send('OK')
 }
 
 const loginUser = (req, res)=>{
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,21 @@
 const express = require('express')
 const dovenv = require('dotenv').config()
+const {errorHandler} = require('./middleware/errorMiddleware')
 const port = process.env.PORT|| 5000
 
+
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
 app.get('/', (req, res)=>{
     res.send('Welcome home!')
 })
 
 app.use('/api/users', require('./routes/userRoute'))
+
+app.use(errorHandler)
 
 app.listen(port, ()=>{
     console.log(`LISTENING ON PORT ${port}`)
