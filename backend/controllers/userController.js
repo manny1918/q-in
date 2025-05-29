b68b115ae6855c253146586ef36fb48dc3a65028
happylearning17 <pedrolopez@amazon.com>
2025-05-29 20:36:34 +0100

    create user model

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,11 +1,40 @@
 const asyncHandler = require('express-async-handler');
+const bcrypt = require('bcrypt')
+
+const User = require('../models/userModel')
 
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
   if (!name || !email || !password) {
     throw new Error('Please include all the fields');
   }
-  res.send('OK');
+
+  const userExists = await User.findOne({email})
+  if(userExists){
+    res.status(400)
+    throw new Error('User already exists')
+  }
+
+  // Hash password
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(password, salt)
+
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword
+  })
+
+  if(user){
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email
+    })
+  }else{
+    res.status(400)
+    throw new Error('Invalid user data')
+  }
 });
 
 const loginUser = asyncHandler(async (req, res) => {
