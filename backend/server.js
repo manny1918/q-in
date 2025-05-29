5469bc39ba49339b8aa5e9d4d465d34dd1c28bde
patito <happylearning777@hotmail.com>
2025-05-29 16:40:10 +0100

    add dotenv

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require('express')
-const app = express()
+const dovenv = require('dotenv').config()
 const port = process.env.PORT|| 5000
+
+const app = express()
 
 app.get('/', (req, res)=>{
     res.send('Welcome home!')
