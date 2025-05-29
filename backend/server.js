7b36388b390b402502ab9be0aab2e32b8cbefdea
patito <happylearning777@hotmail.com>
2025-05-29 16:35:37 +0100

    server basic config

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log('Server')
+const express = require('express')
+const app = express()
+const port = process.env.PORT|| 5000
+
+app.get('/', (req, res)=>{
+    res.send('Welcome home!')
+})
+
+app.listen(port, ()=>{
+    console.log(`LISTENING ON PORT ${port}`)
+})
