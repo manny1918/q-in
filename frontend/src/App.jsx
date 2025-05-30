c054b322cde9c5304332b941c60fb0b82fddbf87
happylearning17 <pedrolopez@amazon.com>
2025-05-30 21:31:55 +0100

    private route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,32 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import { ToastContainer } from 'react-toastify'
-import 'react-toastify/ReactToastify.css'
-import Home from './pages/Home'
-import Register from './pages/Register'
-import Login from './pages/Login'
-import Header from './components/Header'
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/ReactToastify.css';
+import Home from './pages/Home';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Header from './components/Header';
+import NewService from './pages/NewService';
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
-  return <>
-    <Router>
-      <div className="container">
-        <Header />
-        <Routes>
-          <Route path='/register' element={<Register />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/' element={<Home />} />
-        </Routes>
-      </div>
-    </Router>
-    <ToastContainer/>
-  </>
+  return (
+    <>
+      <Router>
+        <div className='container'>
+          <Header />
+          <Routes>
+            <Route path='/register' element={<Register />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/' element={<Home />} />
+            <Route path='/new-service' element={<PrivateRoute />}>
+              <Route path='/new-service' element={<NewService />} />
+            </Route>
+          </Routes>
+        </div>
+      </Router>
+      <ToastContainer />
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/pages/NewService.jsx
@@ -0,0 +1,5 @@
+export default function NewService() {
+    return(
+        <h1>New Ticket</h1>
+    )
+}
