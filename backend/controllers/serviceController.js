05e32399f41300f29944c3e48b78421218d7f266
patito <happylearning777@hotmail.com>
2025-05-30 16:30:56 +0100

    get services

--- backend/controllers/serviceController.js
+++ backend/controllers/serviceController.js
@@ -3,9 +3,17 @@ const User = require('../models/userModel')
 const Service = require('../models/serviceModel')
 
 const getServices = asyncHandler(async (req, res) => {
-    res.status(200).json({
-        message: 'Get tickets'
-    })
+    // Get user using the id in the jwt
+    const user = await User.findById(req.user.id)
+
+    if(!user){
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    const services = await Service.find()
+
+    res.status(200).json(services)
 })
 
 const createService = asyncHandler(async (req, res) => {
