c3076185530c0bda0ce3ee919f457f2b4565063f
patito <happylearning777@hotmail.com>
2025-05-30 11:25:13 +0100

    update home page

--- frontend/pages/Home.jsx
+++ frontend/pages/Home.jsx
@@ -1,20 +1,17 @@
 import { Link } from "react-router-dom";
-import {FaAccusoft, FaSearch} from 'react-icons/fa'
+import {MdAdd, MdAllInclusive} from 'react-icons/md'
 const company = import.meta.env.VITE_COMPANY_NAME || 'DEFAULT COMPANY';
 
 export default function Home() {
 
     return (
-        <>
+                <>
             <section className="heading">
-                <h1>
-                    Welcome to {company}
-                </h1>
-                <p>Please select an option from below</p>
+                <h1>What do you need help with?</h1>
+                <p>Please choose one of the options below</p>
             </section>
-
-            <Link to='/create-service' className='btn btn-reverse'><FaAccusoft/> Create a service</Link>
-            <Link to='/services' className='btn btn-block'> <FaSearch/> Service</Link>
+            <Link to='/new-service' className="btn btn-reverse btn-block"><MdAdd/>Create new service</Link>
+            <Link to='/services' className="btn btn-block"><MdAllInclusive/>View services</Link>
         </>
     )
 }
