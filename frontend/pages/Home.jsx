068bdebf4630129f47f4113c8ffd98988c195bd3
patito <happylearning777@hotmail.com>
2025-05-30 11:16:01 +0100

    reset css

--- frontend/pages/Home.jsx
+++ frontend/pages/Home.jsx
@@ -1,5 +1,20 @@
-export default function Home(){
-    return(
-        <div>Home</div>
+import { Link } from "react-router-dom";
+import {FaAccusoft, FaSearch} from 'react-icons/fa'
+const company = import.meta.env.VITE_COMPANY_NAME || 'DEFAULT COMPANY';
+
+export default function Home() {
+
+    return (
+        <>
+            <section className="heading">
+                <h1>
+                    Welcome to {company}
+                </h1>
+                <p>Please select an option from below</p>
+            </section>
+
+            <Link to='/create-service' className='btn btn-reverse'><FaAccusoft/> Create a service</Link>
+            <Link to='/services' className='btn btn-block'> <FaSearch/> Service</Link>
+        </>
     )
 }
