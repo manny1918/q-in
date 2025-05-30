2becfb615a38367815faa7c7ead44254ebc2653c
patito <happylearning777@hotmail.com>
2025-05-30 10:09:26 +0100

    Add routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,20 @@
-function App() {
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from '../pages/Home'
+import Register from '../pages/Register'
+import Login from '../pages/Login'
 
-  return (
-    <div>My App</div>
-  )
+function App() {
+  return <>
+    <Router>
+      <div className="container">
+        <Routes>
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
+  </>
 }
 
 export default App
