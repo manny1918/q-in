241894256e75a8fbcd5d9f8aec7d6f76d2bf3253
patito <happylearning777@hotmail.com>
2025-05-30 10:23:16 +0100

    create header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,13 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from '../pages/Home'
 import Register from '../pages/Register'
 import Login from '../pages/Login'
+import Header from './components/Header'
 
 function App() {
   return <>
     <Router>
       <div className="container">
+        <Header/>
         <Routes>
           <Route path='/register' element={<Register />} />
           <Route path='/login' element={<Login />} />
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,22 @@
+import { FaSignInAlt, FaSignOutAlt, FaUser, FaUserClock } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+
+export default function Header() {
+    return (
+        <header className="header">
+            <div className="logo">
+                <Link to='/'>Queue Managment System</Link>
+            </div>
+            <ul>
+                <li>
+                    <Link to='/login'>
+                        <FaSignInAlt />Login
+                    </Link></li>
+                <li>
+                    <Link to='/register'>
+                        <FaUser />Register
+                    </Link></li>
+            </ul>
+        </header>
+    )
+}
