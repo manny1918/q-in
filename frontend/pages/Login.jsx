f5014ff945ac32df0baa2dc6953d9ebe18ea3f52
patito <happylearning777@hotmail.com>
2025-05-30 10:47:25 +0100

    Create login page

--- frontend/pages/Login.jsx
+++ frontend/pages/Login.jsx
@@ -1,5 +1,67 @@
-export default function Login(){
-    return(
-        <div>Login</div>
+import { useState } from 'react'
+import { FaSignInAlt } from 'react-icons/fa'
+import { toast } from 'react-toastify'
+
+export default function Login() {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    })
+
+    const {  email, password} = formData
+
+    const onChange = (e) => {
+        setFormData((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value
+        }))
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+        // if(password!==password2) {
+        //     toast.error('The passwords do not match')
+        // }
+    }
+
+    return (
+        <>
+            <section className="heading">
+                <h1>
+                    <FaSignInAlt />Login
+                </h1>
+                <p>Please log in</p>
+            </section>
+
+            <section className="form">
+                <form onSubmit={onSubmit}>
+                    <div className="form-group">
+                        <input
+                            type="email"
+                            id='email'
+                            name='email'
+                            value={email}
+                            className="form-control"
+                            onChange={onChange}
+                            placeholder='Enter your email'
+                            required />
+                    </div>
+                    <div className="form-group">
+                        <input
+                            type="password"
+                            id='password'
+                            name='password'
+                            value={password}
+                            className="form-control"
+                            onChange={onChange}
+                            placeholder='Enter your password'
+                            required />
+                    </div>
+                    <div className="form-group">
+                        <button className="btn btn-block">Submit</button>
+                    </div>
+                </form>
+            </section>
+        </>
     )
 }
