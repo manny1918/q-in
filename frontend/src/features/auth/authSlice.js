cff186d0def4ff5dfbddce57fe717a5cfd1a1407
patito <happylearning777@hotmail.com>
2025-05-30 13:34:18 +0100

    create extra reducers for register

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,4 +1,5 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import authService from './authService'
 
 const initialState = {
     user: null,
@@ -11,7 +12,14 @@ const initialState = {
 export const register = createAsyncThunk(
     'auth/register',
     async (user, thunkAPI) => {
-        console.log(user)
+        try {
+            return await authService.register(user)
+        } catch (error) {
+            const message = (error.response && error.response.data && error.response.data.message)
+                || error.message || error.toString()
+
+            return thunkAPI.rejectWithValue(message)
+        }
     })
 
 export const login = createAsyncThunk(
@@ -25,7 +33,21 @@ export const authSlice = createSlice({
     initialState,
     reducers: {},
     extraReducers: (builder) => {
-
+        builder
+        .addCase(register.pending, (state)=>{
+            state.isLoading = true
+        })
+        .addCase(register.fulfilled, (state, action)=>{
+            state.isLoading = false
+            state.isSuccess = true
+            state.user = action.payload
+        })
+        .addCase(register.rejected, (state, action)=>{
+            state.isLoading = false
+            state.isError = true
+            state.message = action.payload
+            state.user = null
+        })
     }
 })
 
