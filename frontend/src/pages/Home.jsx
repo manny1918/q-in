2c57d5959cd540d8623fbae874525bd3041517f4
patito <happylearning777@hotmail.com>
2025-06-03 15:30:50 +0100

    Add more cards to the Home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,13 @@
 import { Link } from "react-router-dom";
-import { MdAdd, MdList, MdToday, MdSettings, MdViewList } from "react-icons/md";
+import {
+  MdAdd,
+  MdList,
+  MdToday,
+  MdSettings,
+  MdViewList,
+  MdPerson,
+  MdPlayArrow,
+} from "react-icons/md";
 
 const company = import.meta.env.VITE_COMPANY_NAME || "Default Company";
 
@@ -30,6 +38,16 @@ export default function Home() {
       icon: <MdSettings className="icon" />,
       link: "/settings",
     },
+    {
+      label: "Take a Turn",
+      icon: <MdPlayArrow className="icon" />,
+      link: "/take-turn",
+    },
+    {
+      label: "Users",
+      icon: <MdPerson className="icon" />,
+      link: "/users",
+    },
   ];
 
   return (
@@ -48,21 +66,3 @@ export default function Home() {
     </section>
   );
 }
-
-// import { Link } from "react-router-dom";
-// import {MdAdd, MdAllInclusive} from 'react-icons/md'
-// const company = import.meta.env.VITE_COMPANY_NAME || 'DEFAULT COMPANY';
-
-// export default function Home() {
-
-//     return (
-//                 <>
-//             <section className="heading">
-//                 <h1>What do you need help with?</h1>
-//                 <p>Please choose one of the options below</p>
-//             </section>
-//             <Link to='/new-service' className="btn btn-reverse btn-block"><MdAdd/>Create new service</Link>
-//             <Link to='/services' className="btn btn-block"><MdAllInclusive/>View services</Link>
-//         </>
-//     )
-// }
