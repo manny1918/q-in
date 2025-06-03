592f4cfc2641e41222f8aa4c09d1fad1b5b2941e
patito <happylearning777@hotmail.com>
2025-06-03 14:22:45 +0100

    update service form

--- frontend/src/pages/Service.jsx
+++ frontend/src/pages/Service.jsx
@@ -21,7 +21,26 @@ export default function Service() {
         dispatch(getService(params.serviceId))
     }, [isError, message, params.serviceId])
 
+    if(isLoading){
+        return <Spinner/>
+    }
+
+    if(isError){
+        return <h3>Something went wrong</h3>
+    }
+
     return (
-        <h1>Service {params.serviceId}</h1>
+        <div className="service-page">
+            <header className="service-header">
+                <BackButton url='/services'/>
+                <h2>Service ID: {params.serviceId}</h2>
+                <h3>Created on: {new Date(service.createdAt).toLocaleString()}</h3>
+                <hr />
+                <div className="service-desc">
+                    <h3>Service Name: </h3>
+                    <p>${service.serviceName}</p>
+                </div>
+            </header>
+        </div>
     )
 }
