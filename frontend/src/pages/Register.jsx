460914aa595af786ae3728791e6b1324d63bee35
patito <happylearning777@hotmail.com>
2025-06-05 14:02:23 +0100

    update register form

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -3,117 +3,141 @@ import { FaUser } from 'react-icons/fa'
 import { toast } from 'react-toastify'
 import { useSelector, useDispatch } from 'react-redux'
 import { register, reset } from '../features/auth/authSlice'
+import { getServices } from '../features/service/serviceSlice'
 import { useNavigate } from 'react-router-dom'
 import BackButton from '../components/BackButton'
+import Select from 'react-select'
 
 export default function Register() {
-    const [formData, setFormData] = useState({
-        name: '',
-        email: '',
-        password: '',
-        password2: ''
-    })
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
 
-    const { name, email, password, password2 } = formData
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+  })
 
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
+  const [selectedServices, setSelectedServices] = useState([])
 
-    const { user, isLoading, isError, isSuccess, message } = useSelector(state => state.auth)
+  const { name, email, password, password2 } = formData
 
-    useEffect(() => {
-        if (isError) {
-            toast.error(message)
-        }
+  const { services } = useSelector(state => state.service)
+  const { user, isLoading, isError, isSuccess, message } = useSelector(state => state.auth)
 
-        if (isSuccess) {
-            navigate('/users')
-        }
+  useEffect(() => {
+    dispatch(getServices())
+  }, [dispatch])
 
-        dispatch(reset())
-    }, [isError, isSuccess, user, navigate, dispatch])
-
-    const onChange = (e) => {
-        setFormData((prevState) => ({
-            ...prevState,
-            [e.target.name]: e.target.value
-        }))
+  useEffect(() => {
+    if (isError) {
+      toast.error(message)
     }
 
-    const onSubmit = (e) => {
-        e.preventDefault()
-        if (password !== password2) {
-            toast.error('The passwords do not match')
-        } else {
-            const userData = {
-                name,
-                email,
-                password
-            }
-
-            dispatch(register(userData))
-        }
+    if (isSuccess) {
+      navigate('/users')
     }
 
-    return (
-        <>
-            <section className="heading">
-                <BackButton url='/users' />
-                <h1>
-                    <FaUser />Register
-                </h1>
-                <p>Please create an account</p>
-            </section>
+    dispatch(reset())
+  }, [isError, isSuccess, user, navigate, dispatch])
 
-            <section className="form">
-                <form onSubmit={onSubmit}>
-                    <div className="form-group">
-                        <input
-                            type="text"
-                            id='name' name='name'
-                            value={name} className="form-control"
-                            onChange={onChange}
-                            placeholder='Enter your name'
-                            required />
-                    </div>
-                    <div className="form-group">
-                        <input
-                            type="email"
-                            id='email'
-                            name='email'
-                            value={email}
-                            className="form-control"
-                            onChange={onChange}
-                            placeholder='Enter your email'
-                            required />
-                    </div>
-                    <div className="form-group">
-                        <input
-                            type="password"
-                            id='password'
-                            name='password'
-                            value={password}
-                            className="form-control"
-                            onChange={onChange}
-                            placeholder='Enter your password'
-                            required />
-                    </div>
-                    <div className="form-group">
-                        <input
-                            type="password"
-                            id='password2'
-                            name='password2'
-                            value={password2}
-                            className="form-control"
-                            onChange={onChange}
-                            placeholder='Enter confirm your password'
-                            required />
-                    </div>
-                    <div className="form-group">
-                        <button className="btn btn-block">Submit</button>
-                    </div>
-                </form>
-            </section>
-        </>
-    )
+  const onChange = (e) => {
+    setFormData(prev => ({
+      ...prev,
+      [e.target.name]: e.target.value
+    }))
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    if (password !== password2) {
+      toast.error('The passwords do not match')
+      return
+    }
+
+    const userData = {
+      name,
+      email,
+      password,
+      services: selectedServices.map(s => s.value) // Array of ObjectIds
+    }
+
+    dispatch(register(userData))
+  }
+
+  const serviceOptions = (services || []).map(s => ({
+    value: s._id,
+    label: s.serviceName
+  }))
+
+  return (
+    <>
+      <section className="heading">
+        <BackButton url='/users' />
+        <h1><FaUser /> Register</h1>
+        <p>Please create an account</p>
+      </section>
+
+      <section className="form">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              type="text"
+              name="name"
+              value={name}
+              className="form-control"
+              onChange={onChange}
+              placeholder="Enter your name"
+              required />
+          </div>
+          <div className="form-group">
+            <input
+              type="email"
+              name="email"
+              value={email}
+              className="form-control"
+              onChange={onChange}
+              placeholder="Enter your email"
+              required />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              name="password"
+              value={password}
+              className="form-control"
+              onChange={onChange}
+              placeholder="Enter your password"
+              required />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              name="password2"
+              value={password2}
+              className="form-control"
+              onChange={onChange}
+              placeholder="Confirm your password"
+              required />
+          </div>
+
+          <div className="form-group">
+            <label>Select Services:</label>
+            <Select
+              options={serviceOptions}
+              value={selectedServices}
+              onChange={setSelectedServices}
+              isMulti
+              placeholder="Assign services to user..."
+            />
+          </div>
+
+          <div className="form-group">
+            <button className="btn btn-block">Submit</button>
+          </div>
+        </form>
+      </section>
+    </>
+  )
 }
